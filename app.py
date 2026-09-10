9f7174e942189104e26f50f66fa3fdeac6710761
Pranjal Shrestha <135492582+pranjalshrestha@users.noreply.github.com>
2026-09-10 12:42:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from sklearn.metrics import mean_squared_error
 
 st.set_page_config(page_title="BudgetVision", layout="wide")
-st.title("BudgetVision: Automated Budget Variance & Forecasting Tool")
+st.title("AI Financial Intelligence – BudgetVision⁠")
 
 # File uploader
 uploaded_file = st.file_uploader("Upload your CSV file", type="csv")
